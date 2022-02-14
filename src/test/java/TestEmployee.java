4d2047c8ccfb36a69550988faa561646798d0f66
Jakob Nilsson <jakob.nilsson@iths.se>
2022-02-14 14:59:32 +0100

    Green! getEnameTest godkänt

--- src/test/java/TestEmployee.java
+++ src/test/java/TestEmployee.java
@@ -83,6 +83,8 @@ public class TestEmployee {
         String expected = "Nilsson";
 
         String actual = TestObject1.getEname();
+
+        Assertions.assertEquals(expected,actual);
     }
 
 }
