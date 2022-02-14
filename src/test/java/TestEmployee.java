18f203105ed2edd4c289fbfd2bd6494beb5991ad
Jakob Nilsson <jakob.nilsson@iths.se>
2022-02-14 11:08:19 +0100

    RED!

--- src/test/java/TestEmployee.java
+++ src/test/java/TestEmployee.java
@@ -1,7 +1,31 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class TestEmployee {
 
     Employee TestObject;
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("Nu börjar vi: ");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("Nu slutar vi: ");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("Start: ");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("Slut: ");
+
+        TestObject = new Employee("Jakob", "Nilsson", 23, 10.000);
+    }
+
+
 
 }
