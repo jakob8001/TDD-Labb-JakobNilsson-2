262fc9fd17333f55816bf284cc0c78f62aa1de63
Jakob Nilsson <jakob.nilsson@iths.se>
2022-02-14 11:23:48 +0100

    RED!

--- src/test/java/TestEmployee.java
+++ src/test/java/TestEmployee.java
@@ -14,16 +14,23 @@ public class TestEmployee {
         System.out.println("Nu slutar vi: ");
     }
 
-    @BeforeEach
-    public void beforeEach(){
-        System.out.println("Start: ");
-    }
-
     @AfterEach
     public void afterEach(){
         System.out.println("Slut: ");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("Start: ");
 
         TestObject = new Employee("Jakob", "Nilsson", 23, 10.000);
+    }
+
+    @Test
+    public void checkGetUniqueID(){
+        int expected = 1001;
+
+        int actual = TestObject.getUniqueID();
     }
 
 
